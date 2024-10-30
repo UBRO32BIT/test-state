1d41b6494c47db34658bde6a9df46b69a4429bf1
UBRO32BIT <90371372+UBRO32BIT@users.noreply.github.com>
2024-10-30 07:45:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import { Text } from "./Text";
+import { useState } from "react"
+import React from 'react';
+import Axios from "axios";
+import { Client } from "@stomp/stompjs";
+import { Stomp } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+
+const client = new Client({
+  brokerURL: "ws://localhost:8082/ws",
+  debug: console.log,
+});
 
 function App() {
+  const [value, setValue] = useState(0);
+  const increaseValue = () => {
+    setValue(value + 1);
+  }
+  const decreaseValue = () => {
+    setValue(value - 1);
+  }
+  const setValueToZero = () => {
+    setValue(0);
+  }
+  const [showText, setShowText] = useState(false);
+  const [catFact, setCatFact] = useState("");
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact);
+    })
+  }
+
+  const [message, setMessage] = React.useState();
+  var stompClient;
+
+  React.useEffect(() => {
+    const socket = new SockJS("http://localhost:8082/ws")
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function(frame) {
+      console.log(frame);
+      stompClient.subscribe('/user/10/queue/messages', function(result) {
+        console.log(JSON.parse(result.body));
+        setMessage(result.body);
+      })
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div style={{display: 'flex', justifyContent: 'center', paddingTop: '5vh'}}>
+        <button onClick={increaseValue}>Increase</button>
+        <button onClick={decreaseValue}>Decrease</button>
+        <button onClick={setValueToZero}>Set To Zero</button>
+        {value}
+      </div>
+      <div style={{display: 'flex', justifyContent: 'center', paddingTop: '5vh'}}>
+        <button
+        onClick={() => {
+          setShowText(!showText);
+        }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Show Text
+        </button>
+      </div>
+      {showText && <Text/>}
+      <div style={{display: 'flex', justifyContent: 'center', paddingTop: '5vh'}}>
+        <button onClick={fetchCatFact}>Generate Cat Fact</button>
+      </div>
+      <div style={{textAlign: 'center'}}>
+        {catFact}
+      </div>
+      <div style={{display: 'flex', justifyContent: 'center', paddingTop: '5vh'}}>
+          <h1>Tao</h1>
+          <h2>test websocket</h2>        
+      </div>
+      <div>{message}</div>
+    </>
   );
 }
 
